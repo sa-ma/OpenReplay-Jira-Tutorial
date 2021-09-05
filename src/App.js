0583bf2a25d9840f09d4bbc1d172239e172729b4
Samaila Bala <samailabalap@gmail.com>
2021-09-05 03:28:21 +0100

    setup app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tracker from '@openreplay/tracker';
+
+const tracker = new Tracker({
+    projectKey: 'O6Q7rBQdbxGMWgIeL7rd',
+    ingestPoint: 'https://app.openreplaytutorial.ga/ingest',
+});
+tracker.start();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [counter, setCounter] = useState(0);
+
+    const handleClick = (operation = '+') => {
+        if (operation === '-') {
+            if (counter < 0) {
+                throw new Error('Number is less than zero');
+            }
+            setCounter((prevState) => (prevState -= 1));
+            return;
+        }
+        setCounter((prevState) => (prevState += 1));
+    };
+
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <img src={logo} className='App-logo' alt='logo' />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+                    Learn React
+                </a>
+            </header>
+            <main>
+                <button onClick={() => handleClick('-')}>-</button>
+                <p>{counter}</p>
+                <button onClick={() => handleClick()}>+</button>
+            </main>
+        </div>
+    );
 }
 
 export default App;
